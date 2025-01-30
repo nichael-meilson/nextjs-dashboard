3a3e2efe43a9906e461b146b91a36faa270198de
nichael-meilson <michaelshawnneilson@gmail.com>
2025-01-30 20:29:43 +0100

    Chapter 14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -62,6 +62,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     // If a database error occurs, return a more specific error.
     return {
       message: 'Database Error: Failed to Create Invoice.',
+      error: error
     };
   }
  
@@ -98,7 +99,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
-    return { message: 'Database Error: Failed to Update Invoice.' };
+    return { message: 'Database Error: Failed to Update Invoice.', error: error };
   }
  
   revalidatePath('/dashboard/invoices');
