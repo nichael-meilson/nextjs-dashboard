8456258c232ca144774b634c38131d225b091c3d
nichael-meilson <michaelshawnneilson@gmail.com>
2025-01-30 09:23:51 +0100

    Chapter 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -40,7 +40,7 @@ export async function createInvoice(formData: FormData) {
     redirect('/dashboard/invoices');
   }
 
-export async function updateInvoice(id: string, formData: FormData) {
+  export async function updateInvoice(id: string, formData: FormData) {
     const { customerId, amount, status } = UpdateInvoice.parse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
@@ -49,11 +49,16 @@ export async function updateInvoice(id: string, formData: FormData) {
    
     const amountInCents = amount * 100;
    
-    await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
+    try {
+      await sql`
+          UPDATE invoices
+          SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+          WHERE id = ${id}
+        `;
+    } catch (error) {
+      // We'll log the error to the console for now
+      console.error(error);
+    }
    
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
